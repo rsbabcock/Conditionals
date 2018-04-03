e1669c8c9e5cd4090d20c1b70e365406d2453ec0
rbabcock <rachael.s.babcock@gmail.com>
2018-04-03 13:26:46 -0500

    added index.html and main.js, created last name conditional

---
+++ main.js
@@ -0,0 +1,15 @@
+// Exercise: Using Javascript, create a variable to hold your last name. 
+// Using the if-else statement, do the following: 
+// If your last name has 7 or more letters, 
+// console log "I can't pronounce this last name". 
+// Else, console log your last name.
+
+const lastName = "Babcock";
+if (lastName.length > 6) {
+    console.log("I can't pronounce this last name");
+} else { 
+    console.log(lastName);
+}
+
+
+// Challenge: Create a new repository on Github and push the code you wrote up to Github.
